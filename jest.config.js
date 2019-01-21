f694682a5401a23d85788e860df6304f612ab7de
Serge Vartanov <vartanov.s@gmail.com>
2019-01-21 14:14:26 -0800

    Additional Jest Config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src",                     // All TS files are in the src directory
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest",        // Specify use of ts-jest for all .ts/.tsx files
+  clearMocks: true,                                   // Clear mock calls between every test
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],  // Generate coverage report from src dir
+  coverageDirectory: 'coverage',                      // Specify coverage report output dir
+  roots: [ '<rootDir>/src' ],                         // All TS files are in the src directory
+  testRegex: '.+\.test\.tsx?$',                       // Look for file.test.ts/s files
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',                     // Specify use of ts-jest for all .ts/.tsx files
   },
-  "testRegex": ".+\.test\.tsx?$",        // Look for file.test.ts/s files
-  "globals": {
+  verbose: false,                                     // Specify whether to report each indiv test
+  globals: {
     "ts-jest": {
-      "tsConfig": "tsconfig.json",       // Use tsconfig for ts-jest
+      tsConfig: "tsconfig.json",                      // Use tsconfig for ts-jest
     },
   },
-  "moduleFileExtensions": ["ts", "tsx", "js", "jsx"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
